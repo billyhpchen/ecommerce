80a501c71b028cdbc1f30e9353d0ffa14c0061c2
Billy Chen <billy@Billys-MacBook-Pro.local>
2025-11-24 23:07:51 -0800

    add checkout

---
+++ app/checkout/page.tsx
@@ -0,0 +1,8 @@
+export default function Checkout() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-zinc-50 dark:bg-black">
+            <h1 className="text-4xl font-bold mb-8 text-gray-900 dark:text-white">Checkout Page</h1>
+            <p className="text-lg text-gray-600 dark:text-gray-400">This is where the checkout process will take place.</p>
+        </div>
+    )
+}
